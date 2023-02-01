9c3aae10d1f73492fab96b1f79b43ba6296ef9eb
Joe Kennaugh <joebkennaugh@gmail.com>
2023-02-01 00:02:04 +0000

    finished script, all logic now working

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+let container = $(".container");
+let currentDay = $("#currentDay");
+let currentDate;
+
+init();
+
+// function to load on startup, builds the task list and current date
+function init() {
+    container.html("");
+    currentDate = moment().format("dddd, Do of MMMM YYYY");
+    currentDay.text(currentDate);
+
+    for (let i = 0; i<tasks.length; i++){
+        let hourRow = $("<div>").addClass("time-block row").attr("hour-count", i);
+        
+        container.append(hourRow);
+        
+        if(tasks[i][0] <= 12){ var hour = $("<p>").addClass("hour").text(tasks[i][0]+"am"); }
+        else { var hour = $("<p>").addClass("hour").text(tasks[i][0]-12+"pm"); }
+
+        if(tasks[i][0] < moment().hour()){
+            var taskInput = $("<textarea>").addClass("task past");
+        }
+        else if(tasks[i][0] > moment().hour()){
+            var taskInput = $("<textarea>").addClass("task future");
+        }
+        else {
+            var taskInput = $("<textarea>").addClass("task present");
+        }
+
+        let saveButton = $("<button>").addClass("saveBtn");
+        let saveIcon = $("<i>").addClass("fa-solid fa-floppy-disk");
+        saveButton.append(saveIcon);
+        hourRow.append(hour, taskInput, saveButton);
+    }
+}
+
+$(".saveBtn").on("click", function() {
+    //hourCount is used to know where this button corresponds to in the tasks array
+    hourCount = $(this).parent().attr("hour-count");
+
+    tasks[hourCount][1] = $(this).siblings(".task").val();
+    
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    $("#saveNotification").toggleClass("hide");
+    setTimeout(function(){
+        $("#saveNotification").toggleClass("hide");
+    }, 2500);
+})
+
+//updates classes of timeblocks every seconds to ensure is up to date with current hour
+setInterval(function(){
+    for(let i = 0; i<tasks.length; i++){
+        let taskInput = $(`[hour-count=${i}]`).children(".task");
+
+        if(tasks[i][0] < moment().hour()){
+            taskInput.addClass("past").removeClass("future present");
+        }
+        else if(tasks[i][0] > moment().hour()){
+            taskInput.addClass("future").removeClass("past present");
+        }
+        else {
+            taskInput.addClass("present").removeClass("future past");
+        }
+    }
+}, 1000);
---
+++ assets/js/tasks.js
@@ -0,0 +1,16 @@
+let tasks = JSON.parse(localStorage.getItem('tasks'))
+// if nothing is in tasks then creates empty array in format we need
+if(tasks === []){
+    console.log("here");
+    tasks = [
+        [9, ""],
+        [10, ""],
+        [11, ""],
+        [12, ""],
+        [13, ""],
+        [14, ""],
+        [15, ""],
+        [16, ""],
+        [17, ""]    
+    ]
+}
